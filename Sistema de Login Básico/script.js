904157e37f30d7840730269d951ea59d6f716c13
Dourado <fdouradodeczka@gmail.com>
2024-10-22 19:04:16 -0300

    Sistema de Login básico
    Sistema de login básico feito em aula

---
+++ Sistema de Login Básico/script.js
@@ -0,0 +1,21 @@
+var bancoDeDados = [];
+function add(){
+    let usuario = document.getElementById("username").value;
+    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
+    if(pos == -1){
+        bancoDeDados.push(usuario.toUpperCase());
+        alert("Usuário cadastrado com sucesso")
+        document.getElementById("username").value = null;
+    }else{
+        alert("Esse usuário já foi cadastrado")
+    }
+}
+function login(){
+    let usuario = document.getElementById("user").value;
+    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
+    if(pos == -1){
+        alert("Usuário não encontrado")
+    }else{
+        alert("Bem vindo ao sistema")
+    }
+}
